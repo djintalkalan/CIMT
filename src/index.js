9f4b7c96ab72ce1b55c999265fb05f5e2b4616d7
djintalkalan <sunilsangwan89@gmail.com>
2020-04-11 12:58:42 +0530

    Reset Ruby and install sass #sunil

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import '../public/bower_components/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
