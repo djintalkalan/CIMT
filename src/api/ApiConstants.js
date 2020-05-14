9c7a516142aa06e259c98dd65366c3284c5f7c31
djintalkalan <sunilsangwan89@gmail.com>
2020-05-14 22:04:17 +0530

    URL Updated #sunil

--- src/api/ApiConstants.js
+++ src/api/ApiConstants.js
@@ -1,6 +1,6 @@
 
 const baseUrl ="https://jsonplaceholder.typicode.com"
-const baseUrl_heroku ="https://cors-anywhere.herokuapp.com/https://cimt.herokuapp.com"
+const baseUrl_heroku ="https://cimt.herokuapp.com"
 const baseUrl2 ="https://kartforu.com/deepak/shally_maid_api/api"
 
 export const ApiConstants = {
@@ -8,11 +8,11 @@ export const ApiConstants = {
     getPosts:baseUrl+"/posts",
     loginUser1:baseUrl2+"/customer/login.php",
     getUserList:baseUrl_heroku+"/GetAllUser/",
-    loginUser:baseUrl_heroku+"/validateuser/",
+    loginUser:baseUrl_heroku+"/ValidateUser/",
     addUser:baseUrl_heroku+"/Adduser/",
 
-    getRoleList:baseUrl_heroku+"/getallroles/",
-    addRole:baseUrl_heroku+"/addrole/",
+    getRoleList:baseUrl_heroku+"/GetAllRoles/",
+    addRole:baseUrl_heroku+"/AddRole/",
 
     getCaseList:baseUrl_heroku+"/GetAllCase/",
     addCase:baseUrl_heroku+"/AddCase/",
